a4929b9a50b202afa69f10ceb4f45af23e7871d7
Mario Uema <65201817+mariouema1972@users.noreply.github.com>
2023-01-29 00:15:43 -0300

    inicial commit

---
+++ proyecto-final-cv/js/function.js
@@ -0,0 +1,5 @@
+
+ocument.getElementById("boton").onclick = function (){
+    console.log("Capturamos el evento clik");
+    document.getElementById("demo").innerHTML = "Inglés básico, movilidad propia, disponibilidad full time. Salario a convenir"
+}
